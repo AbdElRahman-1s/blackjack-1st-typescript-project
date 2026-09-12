b57a07efafb66686308a933edcc7d0a68a4fb260
AbdElRahman <xinfargo@gmail.com>
2026-09-12 18:26:56 +0300

    Handle Blackjack situation for both player and dealer

--- solution/app.ts
+++ solution/app.ts
@@ -22,16 +22,16 @@ function calculateHandValue(hand: Card[]): number {
     if (card.value === "J" ||
       card.value === "Q" ||
       card.value === "K") {
-        total+=10;
-    }else if(card.value === "A"){
-      if(11+total <= 21){
-        total += 11 ;
-      }else if(11+total >21){
+      total += 10;
+    } else if (card.value === "A") {
+      if (11 + total <= 21) {
+        total += 11;
+      } else if (11 + total > 21) {
         total++;
       }
-    }else {
-        total += Number(card.value);
-      }
+    } else {
+      total += Number(card.value);
+    }
   }
 
   return total;
@@ -40,48 +40,70 @@ function calculateHandValue(hand: Card[]): number {
 
 const prompt = PromptSync();
 
+let playerBlackjack = false;
+let dealerBlackjack = false;
 let playerBusted = false;
 let playerValue = calculateHandValue(playerHand);
+let dealerValue = calculateHandValue(dealerHand);
 
-
-while (true) {
-  const action = prompt("Hit or Stand? ");
-
-  if(action === "stand"){
-    break;
-  }
-
-  if(action === "hit"){
-    playerHand.push(drawCard(deck));
-     playerValue = calculateHandValue(playerHand);
-    if(playerValue > 21){
-      playerBusted=true;
-      break;
-    }
-  }
-
+if (playerValue === 21 && playerHand.length === 2) {
+  playerBlackjack = true;
+}
+if (dealerValue === 21 && dealerHand.length === 2) {
+  dealerBlackjack = true;
 }
 
 
-if (playerBusted) {
- console.log("You lost ):");
+
+if (playerBlackjack || dealerBlackjack) {
+  if (playerBlackjack && dealerBlackjack) {
+  console.log("Tie.");
+} else if (playerBlackjack && !dealerBlackjack) {
+  console.log("BlackJack, Player Win -_-");
+} else if (dealerBlackjack && !playerBlackjack) {
+  console.log("BlackJack, You Lost ):");
+}
 } else {
-  let dealerValue = calculateHandValue(dealerHand);
-  while(dealerValue < 17){
-    dealerHand.push(drawCard(deck));
-    dealerValue = calculateHandValue(dealerHand);
+  while (true) {
+
+    const action = prompt("Hit or Stand? ");
+
+    if (action === "stand") {
+      break;
+    }
+
+    if (action === "hit") {
+      playerHand.push(drawCard(deck));
+      playerValue = calculateHandValue(playerHand);
+      if (playerValue > 21) {
+        playerBusted = true;
+        break;
+      }
+    }
+
   }
 
-  if(dealerValue > 21){
-    console.log("Player Win -_-");
-  }else{
-   if(playerValue > dealerValue){
-    console.log("Player Win -_-");
-   }else if(playerValue < dealerValue){
-    console.log("Dealers Win ):");
-   }else{
-    console.log("Tie.");
-   }
+
+  if (playerBusted) {
+    console.log("You lost ):");
+  } else {
+    dealerValue = calculateHandValue(dealerHand);
+    while (dealerValue < 17) {
+      dealerHand.push(drawCard(deck));
+      dealerValue = calculateHandValue(dealerHand);
+    }
+
+    if (dealerValue > 21) {
+      console.log("Player Win -_-");
+    } else {
+      if (playerValue > dealerValue) {
+        console.log("Player Win -_-");
+      } else if (playerValue < dealerValue) {
+        console.log("Dealers Win ):");
+      } else {
+        console.log("Tie.");
+      }
+    }
   }
 }
 
